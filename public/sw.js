bb711b6780f462688a99a13a46318cb53d65749d
kakashy <josephwawerug@gmail.com>
2021-04-15 20:58:20 +0300

    New norm

---
+++ public/sw.js
@@ -0,0 +1,22 @@
+const preCache = 'pre-cache-v1'
+const assets = [
+    '/',
+    '/index.css',
+    '/index.html',
+    '/index.js'
+]
+
+self.addEventListener('install', installEvent=>{
+    installEvent.waitUntil(
+        caches.open(preCache).then(cache =>{
+            cache.addAll(assets)
+        })
+    )
+})
+self.addEventListener('fetch', fetchEvent=>{
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request)
+        })
+    )
+})
